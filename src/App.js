a6fe3544af265f198ef0a8e492bbdac5243cee4a
hugo nieto <hugocrony@gmail.com>
2023-10-15 19:19:51 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+// Implement a feature to allow item selection with the following requirements:
+// 1. Clicking an item selects/unselects it.
+// 2. Multiple items can be selected at a time.
+// 3. Make sure to avoid unnecessary re-renders of each list item in the big list (performance).
+// 4. Currently selected items should be visually highlighted.
+// 5. Currently selected items' names should be shown at the top of the page.
+//
+// Feel free to change the component structure at will.
+
+const List = ({ items }) => {
+  const [selItems, setSelItems] = useState([]);
+  
+  const onClickSelect = (e) => {
+  	console.log(e.target.innerText);
+    const name = e.target.innerText;
+  	if (selItems.includes(name)) {
+    	setSelItems(selItems.filter(item => item !== name));
+    }
+    else {
+    	setSelItems([...selItems, name]);
+    }  		
+  };   
+  
+	return (
+    <>
+    <div className='selItems'>
+      <h1>You have selected the following items:</h1>
+      <SelectedNamesComponent names={selItems}></SelectedNamesComponent>
     </div>
+    <ul className="List">
+        {items.map(item => (         
+          <li key={item.name} className={`${selItems.includes(item.name)?'selected':''} List__item List__item--${item.color}`} onClick={onClickSelect}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </>
   );
-}
+};
 
-export default App;
+const SelectedNamesComponent = (props) => {
+	const testNames =  props.names;
+	/*console.log(testNames);*/
+	return (
+
+    <ul>
+      {testNames.map(name => (
+        <li key={name}>
+          {name}
+        </li>
+      ))}
+    </ul>      
+	)
+};
+
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import List from './App';
 import reportWebVitals from './reportWebVitals';
+
+// ---------------------------------------
+// Do NOT change anything below this line.
+// ---------------------------------------
+
+const sizes = ['tiny', 'small', 'medium', 'large', 'huge'];
+const colors = ['navy', 'blue', 'aqua', 'teal', 'olive', 'green', 'lime', 'yellow', 'orange', 'red', 'maroon', 'fuchsia', 'purple', 'silver', 'gray', 'black'];
+const fruits = ['apple', 'banana', 'watermelon', 'orange', 'peach', 'tangerine', 'pear', 'kiwi', 'mango', 'pineapple'];
+
+const items = sizes.reduce(
+  (items, size) => [
+    ...items,
+    ...fruits.reduce(
+      (acc, fruit) => [
+        ...acc,
+        ...colors.reduce(
+          (acc, color) => [
+            ...acc,
+            {
+              name: `${size} ${color} ${fruit}`,
+              color,
+            },
+          ],
+          [],
+        ),
+      ],
+      [],
+    ),
+  ],
+  [],
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <List items={items}/>
   </React.StrictMode>
 );
 
